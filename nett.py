c0d26c5d28e7dddc72d47d713a51307cc87ecd7a
Kendrick Grünberg <k.gruenberg@tu-braunschweig.de>
2022-07-25 15:32:31 +0200

    NETT argparse

--- nett.py
+++ nett.py
@@ -17,7 +17,7 @@ This program takes as input:
   - when NEITHER of the two is supplied, a ranked list of entity candidates
     for every table in the corpus is returned
 * various settings as parameters:
-  - ...
+  - ... (see --help)
   - whether the output tables shall be ordered (--ordered) by how well they fit
     (lazy outputting is lost in that case!)
   - whether to use Jaccard (--jaccard) or SBERT (--sbert) for word similarity
@@ -65,15 +65,15 @@ def combine3(dct1: Dict[WikidataItem, float], weight1: float,\
 						.union(set(dct2.keys()))\
 						.union(set(dct3.keys()))
 	result: List[Tuple[float, WikidataItem]] =\
-		[weight1 * dct1.get(wi, 0.0) +\
+		[(weight1 * dct1.get(wi, 0.0) +\
 		 weight2 * dct2.get(wi, 0.0) +\
 		 weight3 * dct3.get(wi, 0.0),\
-		 wi for wi in allWikidataItems]
+		 wi) for wi in allWikidataItems]
 	sort(result, key=lambda tuple: tuple[0], reverse=True)
 	return result
 
 class Table:
-	def __init__(self, surroundingText="", headerRow: List[str],\
+	def __init__(self, surroundingText: str, headerRow: List[str],\
 				 columns: List[List[str]]):
 		"""
 		Initalize a new Table that has already been parsed.
@@ -213,8 +213,121 @@ defaultTableCorpusWithCorrectMappings: List[Tuple[Table, WikidataItem]] =\
 	]
 
 def main():
-	parser = argparse.ArgumentParser(description="NETT")
-	# ToDo: argparse
+	parser = argparse.ArgumentParser(
+		description="NETT - Narrative Entity Type(s) to Tables")
+
+	parser.add_argument(
+    	'entityTypes',
+    	type=str,
+    	help="""A list of entity types.
+    	Ideally they're Wikidata ID's of the form 'Q000000',
+    	otherwise they're strings automatically mapped to Wikidata ID's.
+    	Normally they're the entity types to look for.
+    	When the --stats flag is supplied however, they are interpreted as
+    	the correct entity types for the tables in the corpus (when the corpus
+    	folder is read in alphabetical order).
+    	When no entity types are supplied at all, a ranked list of entity types
+    	is returned for every table in the corpus instead.""",
+    	nargs='*',
+    	metavar='TYPE')
+
+	parser.add_argument('--stats',
+		action='store_true',
+		help="""Interpret the supplied entity types not as those to search for
+		but rather as the correct mappings for the tables in the corpus.
+		The tables in the corpus are still classfied but based on the given
+		correct mappings, statistics are returned instead.
+		Statistics include mean reciprocal rank (MRR), top-k coverage
+		and recall.""")
+
+	parser.add_argument('-k',
+    	type=int,
+    	default=1,
+    	help="""Only return tables for which the entity type searched for was
+    	in the top-k results. By default, k=1 which means that only tables are
+    	returned for which the entity type searched for was the best guess.""",
+    	metavar='K')
+
+	parser.add_argument('--threshold',
+    	type=float,
+    	default=0.0,
+    	help="""Only return tables for which the entity type searched for got
+    	a score of at least this threshold (floating-point number).
+    	This makes sense particularly when used together with the --normalize
+    	flag. When --normalize is set and all weights are kept at their default
+    	value of 1.0, then this value shall be somewhere between 0.0 and 3.0.
+    	This is a filter that is set in addition to the -k filter!""",
+    	metavar='THRESHOLD')
+
+	parser.add_argument('--dont-use-textual-surroundings',
+		action='store_true',
+		help="""Do not take textual surroundings of tables into account.
+		This only has an effect for JSON input.""")
+
+	parser.add_argument('--dont-use-attr-names',
+		action='store_true',
+		help='Do not take the attribute/column names of tables into account.')
+
+	parser.add_argument('--dont-use-attr-extensions',
+		action='store_true',
+		help='Do not take attribute extensions of tables into account.')
+
+	parser.add_argument('--normalize',
+		action='store_true',
+		help="""Whether to normalize the result of each of the 3
+    	approaches into the [0,1] range""")
+
+	parser.add_argument('--textual-surroundings-weight',
+    	type=float,
+    	default=1.0,
+    	help='How to weight the textual surroundings approach.',
+    	metavar='WEIGHTING')
+
+	parser.add_argument('--attr-names-weight',
+    	type=float,
+    	default=1.0,
+    	help='How to weight the attribute names approach.',
+    	metavar='WEIGHTING')
+
+	parser.add_argument('--attr-extensions-weight',
+    	type=float,
+    	default=1.0,
+    	help='How to weight the attribute extensions approach.',
+    	metavar='WEIGHTING')
+
+	parser.add_argument('--corpus',
+    	type=str,
+    	default='',
+    	help="""Path to a folder containing tables as CSV/JSON/TAR files.
+    	Or path to a single TAR file containing tables as CSV/JSON files.
+    	If not specified, a small default corpus is being used.
+    	Note that all tables smaller than 3x3 are rigorously filtered out.
+    	Folders are parsed in alphabetical order.""",
+    	metavar='PATH')
+
+	parser.add_argument('--ordered',
+		action='store_true',
+		help="""When this flag is set, the tables in the output are sorted by
+		how well they score. Beware that lazy output is lost which means that
+		this flag should not be used together with a very big corpus!""")
+
+	parser.add_argument('--verbose', '-v',
+		action='store_true',
+		help='Print verbose info prints to stderr.')
+
+	parser.add_argument('--debug',
+		action='store_true',
+		help='Print debug info prints to stderr.')
+
+	# cf. https://stackoverflow.com/questions/7869345/
+	#     how-to-make-python-argparse-mutually-exclusive-
+	#     group-arguments-without-prefix:
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('--jaccard', action='store_true',
+		help="Use Jaccard index for word similarity of column/attribute names.")
+	group.add_argument('--sbert', action='store_true', default=True,
+		help="Use SBERT for word similarity of column/attribute names.")
+
 	args = parser.parse_args()
 
 	# ToDo: program
