cbd8fbe401219e45aac063e1ac4beeb3db0d99b4
Kendrick Grünberg <k.gruenberg@tu-braunschweig.de>
2022-07-25 17:26:54 +0200

    basic structure of nett_map_dbpedia_classes_to_wikidata.py

--- nett_map_dbpedia_classes_to_wikidata.py
+++ nett_map_dbpedia_classes_to_wikidata.py
@@ -1,1 +1,22 @@
+dbpediaClassesMappedToWikidata: Dict[str, str] = {}
+
+# (1) Extract all DBpedia classes from .owl file:
+
 # ToDo
+
+# (2) Search Wikidata for every DBpedia class name.
+#     Hopefully, one of the search results will have the
+#     P1709 (equivalent class) property defined, containing the
+#     equivalent DBpedia class:
+
+# ToDo
+
+print(\
+	len(\
+		list(filter(lambda v: v != "", dbpediaClassesMappedToWikidata.values()))
+	) +\
+	" out of " + len(dbpediaClassesMappedToWikidata) +\
+	" DBpedia classes mapped to Wikidata entries:"\
+	)
+print("")
+print(dbpediaClassesMappedToWikidata)
