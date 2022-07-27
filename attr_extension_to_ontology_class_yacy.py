e3fccc2aee2751c1f2eb334fb3e1f22a29197520
Kendrick Grünberg <k.gruenberg@tu-braunschweig.de>
2022-07-27 17:27:49 +0200

    written yacy_search_results_descriptions() but does not work very well

--- attr_extension_to_ontology_class_yacy.py
+++ attr_extension_to_ontology_class_yacy.py
@@ -13,3 +13,26 @@
 #
 # API Example:
 #   http://localhost:8090/yacysearch.json?query=plymouth%20satellite
+
+from typing import List
+
+import urllib.parse
+import json
+from urllib.request import urlopen
+
+API_URL = "http://localhost:8090/yacysearch.json?query="
+
+def yacy_search_results_descriptions(search_string: str) -> List[str]:
+    result: List[str] = []
+
+    api_url = API_URL + urllib.parse.quote_plus(search_string)
+    json_result = urlopen(api_url).read().decode('utf-8')
+    parsed_json = json.loads(json_result)
+
+    for yacy_search_result in parsed_json["channels"][0]["items"]:
+        #print(yacy_search_result)
+        search_result_description = yacy_search_result["description"]
+        if search_result_description != "":
+            result.append(search_result_description)
+
+    return result
