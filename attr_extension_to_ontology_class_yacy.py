9e1710bc344cd2615989d5a1dee04f63d21228e1
Kendrick Grünberg <k.gruenberg@tu-braunschweig.de>
2022-07-27 16:51:45 +0200

    added attr_extension_to_ontology_class_yacy.py

---
+++ attr_extension_to_ontology_class_yacy.py
@@ -0,0 +1,15 @@
+# ToDo
+
+# YaCy (pronounced "ya see") is a P2P search engine:
+#   https://en.wikipedia.org/wiki/YaCy
+#   https://yacy.net/
+# 
+# Download:
+#   https://yacy.net/download_installation/
+#
+# API:
+#   https://wiki.yacy.net/index.php/Dev:API
+#   https://wiki.yacy.net/index.php/Dev:APIyacysearch
+#
+# API Example:
+#   http://localhost:8090/yacysearch.json?query=plymouth%20satellite
