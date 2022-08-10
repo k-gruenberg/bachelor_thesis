dc133e7f3cbb2c05da794b387352781211debff4
Kendrick Grünberg <k.gruenberg@tu-braunschweig.de>
2022-08-10 11:57:40 +0200

    refactored attr_extension_to_ontology_class.py: there is now a attr_extension_to_ontology_class() function also callable from outside

--- attr_extension_to_ontology_class.py
+++ attr_extension_to_ontology_class.py
@@ -68,64 +68,84 @@ from WikidataItem import WikidataItem
 
 DEBUG = False
 
-cell_labels: List[str] = sys.argv[1:]  # input values = cell labels
+VERBOSE = (__name__ == "__main__")
 
-VERBOSE = True
+def attr_extension_to_ontology_class(cell_labels: List[str])\
+    -> Dict[WikidataItem, int]:
 
-# "Step 1 - Cell entity annotation:
-#  For each cell label l[i] we derive a list of k possible entity candidates
-#  E[i] using a search-based disambiguation method [5]. We set k = 10 in our
-#  experiments." [Zwicklbauer et al.]
+    # "Step 1 - Cell entity annotation:
+    #  For each cell label l[i] we derive a list of k possible entity candidates
+    #  E[i] using a search-based disambiguation method [5]. We set k = 10 in our
+    #  experiments." [Zwicklbauer et al.]
 
-entity_candidates_per_label: Dict[str, List[WikidataItem]] =\
-    {cell_label: WikidataItem.get_items_matching_search_string(cell_label)\
-     for cell_label in cell_labels}
+    entity_candidates_per_label: Dict[str, List[WikidataItem]] =\
+        {cell_label: WikidataItem.get_items_matching_search_string(cell_label)\
+         for cell_label in cell_labels}
 
-if VERBOSE:
-	print("\n##### Finished Step 1 - Cell entity annotation: #####")
-	for cell_label, entity_candidates in entity_candidates_per_label.items():
-		print(cell_label + ": " + str([str(ec) for ec in entity_candidates]))
+    if VERBOSE:
+    	print("\n##### Finished Step 1 - Cell entity annotation: #####")
+    	for cell_label, entity_candidates in entity_candidates_per_label.items():
+    		print(cell_label + ": " + str([str(ec) for ec in entity_candidates]))
 
-# "Step 2 - Entity-type resolution:
-#  For each entity candidate e[i]^k in E[i] a set of types is retrieved by
-#  following the rdf:type and dcterms:subject relations yielding the set of
-#  types T[i]^k." [Zwicklbauer et al.]
+    # "Step 2 - Entity-type resolution:
+    #  For each entity candidate e[i]^k in E[i] a set of types is retrieved by
+    #  following the rdf:type and dcterms:subject relations yielding the set of
+    #  types T[i]^k." [Zwicklbauer et al.]
 
-types_per_entity_candidate_per_label: Dict[str, Dict[WikidataItem, List[str]]]\
-    = {cell_label: {entity_candidate: entity_candidate.get_property("P31")\
-                    for entity_candidate in entity_candidates}\
-    for cell_label, entity_candidates in entity_candidates_per_label.items()}
-# note: P31 = Wikidata's "instance of" property
+    types_per_entity_candidate_per_label: Dict[str, Dict[WikidataItem, List[str]]]\
+        = {cell_label: {entity_candidate: entity_candidate.get_property("P31")\
+                        for entity_candidate in entity_candidates}\
+        for cell_label, entity_candidates in entity_candidates_per_label.items()}
+    # note: P31 = Wikidata's "instance of" property
 
-if VERBOSE:
-    print("\n##### Finished Step 2 - Entity-type resolution: #####")
+    if VERBOSE:
+        print("\n##### Finished Step 2 - Entity-type resolution: #####")
+        for cell_label, types_per_entity_candidate in\
+            types_per_entity_candidate_per_label.items():
+            for entity_candiate, types in types_per_entity_candidate.items():
+            	print(cell_label + ": " + entity_candiate.entity_id + ": "\
+            		+ str(types))
+
+
+    # "Step 3 - Type aggregation:
+    #  The types assigned to the table header are the t types that occur most
+    #  frequently in the set of all types of all cells Union[i] Union[k] T[i]^k.
+    #  We set t = 1 in our experiments, e.g. only use the most frequent type as
+    #  result." [Zwicklbauer et al.]
+
+    set_of_all_types_of_all_cells_with_frequency: Dict[str, int] =\
+        defaultdict(lambda: 0)
+
     for cell_label, types_per_entity_candidate in\
         types_per_entity_candidate_per_label.items():
-        for entity_candiate, types in types_per_entity_candidate.items():
-        	print(cell_label + ": " + entity_candiate.entity_id + ": "\
-        		+ str(types))
+        for entity_candidate, types in types_per_entity_candidate.items():
+            if types is not None:
+                for _type in types:
+                    set_of_all_types_of_all_cells_with_frequency[_type] += 1
+
+    if VERBOSE:
+        print("\n##### Finished Step 3 - Type aggregation: #####")
+
+    # Transform the String names of the Wikidata types
+    #   into WikidataItems before returning them:
+    set_of_all_types_of_all_cells_with_frequency: Dict[WikidataItem, int] =\
+        {WikidataItem(_type): frequency for _type, frequency in\
+        set_of_all_types_of_all_cells_with_frequency.items()}
+
+    return set_of_all_types_of_all_cells_with_frequency
 
 
-# "Step 3 - Type aggregation:
-#  The types assigned to the table header are the t types that occur most
-#  frequently in the set of all types of all cells Union[i] Union[k] T[i]^k.
-#  We set t = 1 in our experiments, e.g. only use the most frequent type as
-#  result." [Zwicklbauer et al.]
+def main():  # ToDo
+    cell_labels: List[str] = sys.argv[1:]  # input values = cell labels
 
-set_of_all_types_of_all_cells_with_frequency: Dict[str, int] =\
-    defaultdict(lambda: 0)
+    set_of_all_types_of_all_cells_with_frequency: Dict[WikidataItem, int] =\
+        attr_extension_to_ontology_class(cell_labels)
 
-for cell_label, types_per_entity_candidate in\
-    types_per_entity_candidate_per_label.items():
-    for entity_candidate, types in types_per_entity_candidate.items():
-        if types is not None:
-            for _type in types:
-                set_of_all_types_of_all_cells_with_frequency[_type] += 1
-
-if VERBOSE:
-    print("\n##### Finished Step 3 - Type aggregation: #####")
-
-for _type, frequency in\
-    sorted(set_of_all_types_of_all_cells_with_frequency.items(),\
+    for _type, frequency in\
+        sorted(set_of_all_types_of_all_cells_with_frequency.items(),\
         key=lambda pair: pair[1], reverse=True):
-    print("(" + str(frequency) + ") " + _type + " (" + WikidataItem(_type).get_label() + ")")
+        print("(" + str(frequency) + ") " + _type.entity_id +\
+            " (" + _type.get_label() + ")")
+
+if __name__ == "__main__":
+    main()
