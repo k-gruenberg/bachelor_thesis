f93ebf04d8bf8ea1de4ef897ea907bdd857e608f
Kendrick Grünberg <k.gruenberg@tu-braunschweig.de>
2022-08-26 17:22:00 +0200

    added version with pre- and post-modifier to mongo_webisadb.js

--- mongo_webisadb.js
+++ mongo_webisadb.js
@@ -53,3 +53,60 @@ db.getCollectionNames().forEach(function(collectionName) {
 //  | vader |  |  | band |  | 34
 //  | vader |  |  | band |  | 34
 
+
+// (7) Here is a version that also considers pre- and post-modifier:
+
+let searchTermPreMod = "darth";
+let searchTerm = "vader";
+let searchTermPostMod = "";
+let topK = 25;
+let res = []; // sorted results
+let collectionsDoneCounter = 0;
+db.getCollectionNames().forEach(function(collectionName) {
+    db[collectionName].find({instance: searchTerm}).forEach(function(item) {
+        item.modifications.forEach(function(m) {
+            let indexToInsert = 0;
+            while (indexToInsert < res.length && res[indexToInsert][6] >= m.frequency) {indexToInsert++;}
+            if ((m.ipremod == searchTermPreMod) && (m.ipostmod == searchTermPostMod)) {
+                res.splice(indexToInsert, 0, [m.ipremod, item.instance, m.ipostmod, m.cpremod, item.class, m.cpostmod, m.frequency]);
+            }
+        });
+    });
+    collectionsDoneCounter++;
+    console.log("");
+    console.log("After searching in " + collectionsDoneCounter + " collections:")
+    for (let i = 0; i < Math.min(topK, res.length); i++) {
+        console.log(res[i][0] + " | " + res[i][1] + " | " + res[i][2] + " | " + res[i][3] + " | " + res[i][4] + " | " + res[i][5] + " | " + res[i][6]);
+    }
+});
+
+// (8) Result:
+// [...]
+//
+// After searching in 1406 collections:
+// darth | vader |  | star wars | character |  | 167
+// darth | vader |  | star wars | character |  | 167
+// darth | vader |  |  | character |  | 83
+// darth | vader |  |  | character |  | 83
+// darth | vader |  |  | villain |  | 43
+// darth | vader |  |  | villain |  | 43
+// darth | vader |  |  | none |  | 41
+// darth | vader |  |  | none |  | 41
+// darth | vader |  |  | dad |  | 34
+// darth | vader |  | iconic | character |  | 34
+// darth | vader |  | iconic | character |  | 34
+// darth | vader |  |  | dad |  | 34
+// darth | vader |  |  | dad | like any otherexcept | 29
+// darth | vader |  |  | dad | like any otherexcept | 29
+// darth | vader |  | good | father |  | 21
+// darth | vader |  | villains playable | character |  | 21
+// darth | vader |  |  | great |  | 21
+// darth | vader |  | villains playable | character |  | 21
+// darth | vader |  | good | father |  | 21
+// darth | vader |  |  | great |  | 21
+// darth | vader |  | bad | guy |  | 20
+// darth | vader |  | bad | guy |  | 20
+// darth | vader |  | unwanted | change |  | 18
+// darth | vader |  | unwanted | change |  | 18
+// darth | vader |  | other star wars | character |  | 17
+
